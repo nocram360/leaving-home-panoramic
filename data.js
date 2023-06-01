2540631e08f79ab390657ef42b1aeec13e80653b
Nocram <135268121+nocram360@users.noreply.github.com>
2023-06-02 00:40:39 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-leaving-home-panoramic",
+      "name": "Leaving home panoramic",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.6595196379805799,
+        "pitch": -0.011136426264737764,
+        "fov": 1.0937148614522232
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.19983320257665405,
+          "pitch": -0.07905932927782544,
+          "title": "Info",
+          "text": "Single 360° photo taken in Udine, Italy, on 27 May 2023."
+        }
+      ]
+    }
+  ],
+  "name": "Leaving home panoramic",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
